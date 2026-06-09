a48c8d665d7d7e24b7c8e33cc9f07126598c4736
Ashutosh Kumar <ashutosh.ac.kumar@oracle.com>
2026-06-09 18:51:55 +0530

    JOBOO-BACKEND : first draft

---
+++ src/main/java/com/RoadToSDE3/JobManagementMVP/JobManagementMvpApplication.java
@@ -0,0 +1,13 @@
+package com.RoadToSDE3.JobManagementMVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagementMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagementMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RoadToSDE3/JobManagementMVP/JobManagementMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RoadToSDE3.JobManagementMVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagementMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
